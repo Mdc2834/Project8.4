b457bfc0567a7bd2426ba446d5d1e14b6ff5ba10
Mdc2834 <145085777+Mdc2834@users.noreply.github.com>
2023-09-15 19:27:11 -0400

    Add files via upload

--- Doodlebug.java
+++ Doodlebug.java
@@ -1,101 +1,63 @@
-import java.util.Random;
-
 /** Doodlebug.java
  *  This class defines a Doodlebug's attributes and behavior.
  */
-public class Doodlebug extends Organism
-{
-	final static int starvationThreshold = 3;
-	int timeSinceEat;
+public class Doodlebug extends Organism {
+  /** Constant defining the number of moves required before a Doodlebug
+   *   can breed. */
+	public static final int DOODLEBREED = 4;	
 
-	/**
-	 * Empty Constructor
+  /** Constant defining the number of moves a Doodlebug can make without
+   *   eating before it starves. */
+	public static final int DOODLESTARVE = 8;	
+
+  /** Number of moves since last eating */
+	private int starveTicks;		
+
+	/** Empty Constructor
 	 */
-	public Doodlebug()
-	{
-		super();
-		this.symbol = 'X';
-		this.breedThreshold = 8;
-		this.timeSinceEat = 0;
+ 	public Doodlebug() {
+ 	   // ITP 220 - complete this code
 	}
 
-	/**
-	 * Complete Constructor.
-	 * @param row Row position of new Doodlebug
-	 * @param col Column position of new Doodlebug
+  /** Argument constructor
+   * @param world - World grid on which this Doodlebug lives
+   * @param x - Initial x coordinate position of this Doodlebug
+   * @param y - Initial y coordinate position of this Doodlebug
+   */
+	public Doodlebug(World world, int x, int y) {
+	   // ITP 220 - complete this code
+	}
+ 
+	/** Method defining what happens when a Doodlebug moves.
+	 *  This method must randomly search all adjacent locations for a cell
+	 *   occupied by an Ant (it wants to move where it can eat).
+	 *  If a cell with an Ant is not found, this Doodlebug moves as an Ant,
+	 *   picking a single random location and not moving if this chosen
+	 *   location is not valid or is occupied.
 	 */
-	public Doodlebug(int row, int col)
-	{
-		super(row, col);
-		this.symbol = 'X';
-		this.breedThreshold = 8;
-		this.timeSinceEat = 0;
+	public void move() {
+	   // ITP 220 - complete this code
 	}
 
-	/**
-	 * Copy Constructor
-	 * @param d Doodlebug to be copied
+	/** Method defining what happens when a Doodlebug breeds. 
+	 *  - This method must check that the Doodlebug is able to breed and 
+	 *     may reset it if the Doodlebug breeds successfully.
+	 *  - The Doodlebug must search all locations around it randomly.
 	 */
-	public Doodlebug(Doodlebug d)
-	{
-		super(d);
-		this.symbol = d.symbol;
-		this.breedThreshold = d.breedThreshold;
-		this.timeSinceEat = d.timeSinceEat;
+	public void breed() {
+	   // ITP 220 - complete this code
 	}
 
-	/**
-	 * Takes int 1, 2, 3, or 4, and moves bug up, right, down, or left respectively.
-	 * @param direction The direction in which to move the bug.
+	/** Method to determine whether the Doodlebug is currently starving.
+	 * @return boolean - TRUE if the Doodlebug is starving
 	 */
-	public void moveBug(int direction)
-	{
-		if (direction < 1 || direction > 4)
-		{
-			System.out.println("Error: Improper moveBug parameter.");
-			System.exit(0);
-		}
-
-		switch (direction) {
-			case 1: World.field[this.getRowPosition() - 1][this.getColPosition()] = null; //Remove Ant
-				World.field[this.getRowPosition() - 1][this.getColPosition()] = this; //Copy over DB
-				World.field[this.getRowPosition()][this.getColPosition()] = null; //Remove old DB
-				this.setRowPosition(this.getRowPosition() - 1);
-				break;
-
-			case 2: World.field[this.getRowPosition()][this.getColPosition() + 1] = null; //Remove Ant
-				World.field[this.getRowPosition()][this.getColPosition() + 1] = this; //Copy over DB
-				World.field[this.getRowPosition()][this.getColPosition()] = null; //Remove old DB
-				this.setColPosition(this.getColPosition() + 1);
-				break;
-
-			case 3: World.field[this.getRowPosition() + 1][this.getColPosition()] = null; //Remove Ant
-				World.field[this.getRowPosition() + 1][this.getColPosition()] = this; //Copy over DB
-				World.field[this.getRowPosition()][this.getColPosition()] = null; //Remove old DB
-				this.setRowPosition(this.getRowPosition() + 1);
-				break;
-
-			case 4: World.field[this.getRowPosition()][this.getColPosition() - 1] = null; //Remove Ant
-				World.field[this.getRowPosition()][this.getColPosition() - 1] = this; //Copy over DB
-				World.field[this.getRowPosition()][this.getColPosition()] = null; //Remove old DB
-				this.setColPosition(this.getColPosition() - 1);
-				break;
-		}
+	public boolean starve()	{
+	   // ITP 220 - complete this code
 	}
 
-	/**
-	 * Returns the position, four flag values, time since last ate,
-	 * and time since last bred as a String.
-	 * @return String of information and values about Doodlebug
+	/** Returns a displayable character for this Doodlebug (represented by "X")
 	 */
-	public String toString()
-	{
-		return "Doodlebug at " +  this.getRowPosition() + ", " + this.getColPosition() + "\n" +
-				"topFlag " + this.topFlag + "\n" +
-				"rightFlag " + this.rightFlag + "\n" +
-				"bottomFlag " + this.bottomFlag + "\n" +
-				"leftFlag " + this.leftFlag + "\n" +
-				"timeSinceEat: " + this.timeSinceEat + "\n" +
-				"timeSinceBreed: " + this.timeSinceBreed;
+	public String getPrintableChar()	{
+		return "X";
 	}
 }
